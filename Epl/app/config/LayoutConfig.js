08dbbf39447234f853acbae698e6a811810110a0
Lee <9105lgm@xingxingmobility.com>
2019-11-11 17:43:24 +0900

    SafeArea api 사용 Test

--- Epl/app/config/LayoutConfig.js
+++ Epl/app/config/LayoutConfig.js
@@ -1,10 +1,22 @@
-import { Dimensions } from 'react-native'
+import { Dimensions, Platform } from 'react-native'
+import SafeArea from 'react-native-safe-area'
 
 const screenDimensions = Dimensions.get('window');
 const LayoutConfig = {
     SCREEN_WIDTH: screenDimensions.width,
     SCREEN_HEIGHT: screenDimensions.height,
+    
+    SCREEN_SAFE_TOP: 20,
+    SCREEN_SAFE_BOTTOM: 20
 
 }
+
+if( Platform.OS === 'ios' ){
+    SafeArea.getSafeAreaInsetsForRootView().then((result)=>{
+        LayoutConfig.SCREEN_SAFE_TOP = result.safeAreaInsets.top;
+        LayoutConfig.SCREEN_SAFE_BOTTOM = result.safeAreaInsets.bottom;
+    })
+}
+
 
 export default LayoutConfig;
